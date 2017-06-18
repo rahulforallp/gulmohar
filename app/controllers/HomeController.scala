66833d363b2d01e65cbfc05ba52f8a1d931952b5
rahulforallp <rahul.kumar@knoldus.in>
2017-06-18 21:08:30 +0530

    signup and article upload completed

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import java.io.{BufferedReader, File, FileReader, IOException}
+import java.io.{BufferedReader, File, FileInputStream, FileReader, IOException}
 import java.sql.ResultSet
 
 import play.api.data.Form
@@ -10,7 +10,7 @@ import javax.inject._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, Controller}
 import database.JDBCConnection
-import models.Authors
+import models.{Article, Authors}
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -39,11 +39,20 @@ class HomeController @Inject()(val messagesApi: MessagesApi) extends Controller 
       "email" -> nonEmptyText
     )(Authors.apply)(Authors.unapply))
 
+  val articleForm=Form(
+    mapping(
+      "email" -> nonEmptyText,
+      "title" -> nonEmptyText,
+      "body" -> nonEmptyText
+    )(Article.apply)(Article.unapply)
+  )
   def index = {
     Action {
-      val resultSet = JDBCConnection.executeQuery("select * from authors")
-      val listAuthor = getAuthors(resultSet, Nil)
-      Ok(views.html.index(listAuthor))
+      val resultSetAuthor = JDBCConnection.executeQuery("select * from authors")
+      val resultSetArticle = JDBCConnection.executeQuery("select * from articles")
+      val listAuthor = getAuthors(resultSetAuthor, Nil)
+      val listArticle = getArticles(resultSetArticle, Nil)
+      Ok(views.html.index(listAuthor,listArticle))
     }
   }
 
@@ -61,7 +70,11 @@ class HomeController @Inject()(val messagesApi: MessagesApi) extends Controller 
           "create table if not exists authors(name varchar,designation varchar,about varchar," +
           "email " +
           "varchar)")
+      JDBCConnection
+        .execute(
+          "create table if not exists articles(email varchar,title varchar,body varchar)")
       JDBCConnection.execute("insert into authors values('name1','designation1','about1','email1')")
+      JDBCConnection.execute("insert into articles values('email1','title1','body1')")
      /* JDBCConnection.execute("insert into authors values('name2','designation2','about2','email2')")
       JDBCConnection.execute("insert into authors values('name3','designation3','about3','email3')")
       JDBCConnection.execute("insert into authors values('name4','designation4','about4','email4')")
@@ -76,46 +89,15 @@ class HomeController @Inject()(val messagesApi: MessagesApi) extends Controller 
     }
   }
 
-
   def authors = {
     Action {
-      Ok(views.html.author(signUpForm))
+      Ok(views.html.author(signUpForm,articleForm))
     }
   }
 
-  def upload = Action(parse.multipartFormData) { request =>
-    request.body.file("article").map { article =>
-      import java.io.File
-      val filename = article.filename
-      val contentType = article.contentType
-      article.ref.moveTo(new File(s"/tmp/article/$filename"))
-      Ok(readFile(s"/tmp/article/$filename"))
-    }.getOrElse {
-      Redirect(routes.HomeController.authors()).flashing(
-        "error" -> "Missing file")
-    }
-  }
-
-  def readFile(filePath:String): String ={
-    var br:BufferedReader = null;
-    var  fr :FileReader= null;
-
-    try {
-
-      fr = new FileReader(filePath);
-      br = new BufferedReader(fr);
-
-      var sCurrentLine : String ="" ;
-var body ="";
-
-      while ((sCurrentLine = br.readLine()) != null) {
-        body+=sCurrentLine;
-      }
-return body;
-    } catch  {
-case e:IOException => e.printStackTrace();
-        return "";
-
+  def errorPage = {
+    Action {
+      Ok(views.html.errorPage())
     }
   }
 
@@ -125,6 +107,15 @@ case e:IOException => e.printStackTrace();
       val resultSet = JDBCConnection.executeQuery("select * from authors")
       val listAuthor = getAuthors(resultSet, Nil)
       Ok(listAuthor.mkString(","))
+    }
+  }
+
+  def getAllArticles = {
+    Action {
+      Logger.info("Retrieving All Articles.")
+      val resultSet = JDBCConnection.executeQuery("select * from articles")
+      val listArticles = getArticles(resultSet, Nil)
+      Ok(listArticles.mkString(","))
     }
   }
 
@@ -138,7 +129,7 @@ case e:IOException => e.printStackTrace();
         signUpForm.bindFromRequest.fold(
           formWithErrors => {
             Logger.error("Sign-up badRequest.")
-            Future(BadRequest(views.html.author(formWithErrors)))
+            Future(BadRequest(views.html.errorPage()))
           },
           validData => {
             println(validData.name + " : " + validData.email+" : "+validData.designation+" : "+validData.about)
@@ -158,6 +149,35 @@ case e:IOException => e.printStackTrace();
     }
   }
 
+  /**
+   * Create an Action for aricle upload option
+   */
+  def articleUpload: Action[AnyContent] = {
+    Action.async {
+      implicit request =>
+        Logger.debug("aricle upload in progress. ")
+        articleForm.bindFromRequest.fold(
+          formWithErrors => {
+            Logger.error("aricle upload badRequest.")
+            Future(BadRequest(views.html.errorPage()))
+          },
+          validData => {
+            if (JDBCConnection
+              .execute(
+                "insert into articles values('" + validData.email + "','" + validData.title +
+                "','" + validData.body + "')")) {
+              Logger.info("Article recorded successfully.")
+              Future.successful(Redirect(routes.HomeController.authors()))
+            }
+            else {
+              Logger.error("Article not recorded successfully.")
+              Future.successful(Redirect(routes.HomeController.authors()))
+            }
+          }
+        )
+    }
+  }
+
   def getAuthors(resultSet: ResultSet, authorList: List[Authors]): List[Authors] = {
     if (resultSet.next()) {
       val author = Authors(resultSet.getString(1),
@@ -167,6 +187,17 @@ case e:IOException => e.printStackTrace();
       getAuthors(resultSet, authorList :+ author)
     } else {
       authorList
+    }
+  }
+
+  def getArticles(resultSet: ResultSet, articleList: List[Article]): List[Article] = {
+    if (resultSet.next()) {
+      val article = Article(resultSet.getString(1),
+        resultSet.getString(2),
+        resultSet.getString(3))
+      getArticles(resultSet, articleList :+ article)
+    } else {
+      articleList
     }
   }
 
